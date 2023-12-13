1f4b5f27b63cd5e9c1e7bac3813fb9b9eda42485
Eric <throwawayehcksdee@gmail.com>
2023-12-12 20:37:43 -0500

    mdbf plot

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set_theme()
 
 def read_csv(name):
 	url = f"./datasets"
@@ -18,19 +21,34 @@ st.markdown(
     
     3. Where is funding being allocated?
     
-    # Reliablity
+    ## Reliablity
     
-    ## Mean Distance Between Failures
+    ### Mean Distance Between Failures
     
-    - Mean Distance Between Failures
-    	- Metric for the aggregate distance between mechanical faliures of a train car during operation.
-    	- Each train car used by the MTA:
-     		- Recorded MDBF 
-       		- MDBF goal 
-        - Using both of these values we can calculate the residual 
-        - How well did the fleet meet its reliability goals?
+	- Metric for the aggregate distance between mechanical faliures of a train car during operation.
+	- Each train car used by the MTA:
+		- Recorded MDBF 
+		- MDBF goal 
+	- Using both of these values we can calculate the residual 
+	- How well did the fleet meet its reliability goals?
     """
 )
+
+mdbf = read_csv("MDBF")
+mdbf["Year"] = pd.to_datetime(mdbf["Month"]).dt.year
+mdbf["Month"] = pd.to_datetime(mdbf["Month"]).dt.strftime("%m/%Y")
+mdbf["MDBF Residual"]=(mdbf["MDBF Value"] - mdbf["MDBF Goal"]) / mdbf["MDBF Goal"]
+mdbf = mdbf.sort_values(by=['Year', 'Month'])
+mdbf[["Month", "MDBF Value", "MDBF Goal", "MDBF Residual"]].head()
+
+fig, ax = plt.subplots(figsize=(12, 6))
+mdbf_data = mdbf[mdbf["Fleet Type"] == "Fleet-wide"]
+ax.axhline(0, xmin=0, xmax=1, color="black")
+sns.scatterplot(x="Month", y="MDBF Residual", hue="Fleet Type", data=df, ax=ax, zorder=100)
+ax.set_xlabel("Date")
+fig.autofmt_xdate()
+st.pyplot(fig)
+
 # st.header("Mean Distance Between Failures")
 st.markdown(
 	"""
